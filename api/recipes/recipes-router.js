261a2342b46233dcbc7e1ce8dfab8de0a183594f
harvey magana <heyharv2012@gmail.com>
2021-02-07 09:45:06 -0800

    Connected the recipes and ingredients router files to server, created started code in each of the recipes and ingredients router files.

---
+++ api/recipes/recipes-router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const Recipes = require('./recipes-model.js');
+
+const router = express.Router();
+
+/**
+ * include 
+ * router.get('/', (req, res) => {})
+ * router.get('/:id', (req, res) => {})
+ * router.post('/', (req, res) => {})
+ * router.put('/:id', (req, res) => {})
+ * router.delete('/:id', (req, res) => {})
+ */
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,20 @@
 // connect to recipes-router.js
 // connect to ingredients-router.js
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+const RecipesRouter = require('./recipes/recipes-router.js');
+const IngredientsRouter = require('./ingredients/ingredients-router.js');
+
+server.use(express.json());
+//server.use('/api/recipes'. RecipesRouter);
+//server.use('/api/ingredients', IngredientsRouter);
+
+server.get('/', (req, res) => {
+    res.send(`<h2>DB4 Recipes schema assignment</h2>`);
+  });
+  
+
+module.exports = server;
